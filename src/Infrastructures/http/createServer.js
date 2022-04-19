1fc12b272d29e6866ba0758fee26f3f24e5db361
Muhammad Davi <davi_muhammad@yahoo.com>
2022-04-19 13:56:30 +0700

    update fitur hello world hello

--- src/Infrastructures/http/createServer.js
+++ src/Infrastructures/http/createServer.js
@@ -53,7 +53,7 @@ const createServer = async (container) => {
     method: 'GET',
     path: '/hello',
     handler: () => ({
-      value: 'Hello world today!',
+      value: 'Hello world hello!',
     }),
   });
 
