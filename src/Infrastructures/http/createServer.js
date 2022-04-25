83743c1584d810e6e366681a525e704e8227a8f8
Muhammad Davi <davi_muhammad@yahoo.com>
2022-04-25 10:46:38 +0700

    fixing ci

--- src/Infrastructures/http/createServer.js
+++ src/Infrastructures/http/createServer.js
@@ -53,7 +53,7 @@ const createServer = async (container) => {
     method: 'GET',
     path: '/hello',
     handler: () => ({
-      value: 'Hello World!',
+      value: 'Hello World, I am Davi!',
     }),
   });
 
